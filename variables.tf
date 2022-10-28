ce2f029de18577035522716e1381cb4e54c60eaa
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-10-28 17:24:12 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,36 @@
- variable "ec2_name" {
-   description = "ec2 instance tag Name"
-   default = "china-terraform-core-ec2"
- }
+variable "region" {
+  description = "region"
+  default = "ap-northeast-2"
+}
 
- variable "subnet_id" {
-   description = "subnet identity"
-   default = "subnet-03f7963d2c8889cd5"
- }
+variable "ec2_name" {
+  description = "ec2 instance tag Name"
+  default = "devos-demo-terraform-core-ec2"
+}
 
- variable "security_groups" {
-   description = "security group ids"
-   type = list(string)
-   default = ["sg-067ee7595b22f17f8"]
- }
+variable "subnet_id" {
+  description = "subnet identity"
+  default = "subnet-03f7963d2c8889cd5"
+}
 
- variable "key_pair" {
-   description = "pem key pair name"
-   default = "devos-chaina-service"
- }
+variable "security_groups" {
+  description = "security group ids"
+  type = list(string)
+  default = ["sg-03c12140254b38dc3"]
+}
 
- variable "instance_type" {
-   description = "ec2 instance type"
-   default = "t2.micro"
- }
+variable "key_pair" {
+  description = "pem key pair name"
+  default = "devos-seoul-keypair"
+}
 
- variable "assign_ip" {
-   description = "use assign ip"
-   type = bool
-   default = true
- }
+variable "instance_type" {
+  description = "ec2 instance type"
+  default = "t2.micro"
+}
+
+variable "assign_ip" {
+  description = "use assign ip"
+  type = bool
+  default = true
+}

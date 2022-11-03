de355f03699f2a56312e103e40d05e227b8911b2
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-11-03 11:06:50 +0900

    Create variables.tf

---
+++ dir1/variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  description = "region"
+  default = "ap-northeast-2"
+}
+
+variable "ec2_name" {
+  description = "ec2 instance tag Name"
+  default = "devos-demo-terraform-core-ec2-0824"
+}
+
+variable "subnet_id" {
+  description = "subnet identity"
+  default = "subnet-1af11756"
+}
+
+variable "security_groups" {
+  description = "security group ids"
+  type = list(string)
+  default = ["sg-01f61050e58083d6e"]
+}
+
+variable "key_pair" {
+  description = "pem key pair name"
+  default = "hyein"
+}
+
+variable "instance_type" {
+  description = "ec2 instance type"
+  default = "t2.micro"
+}
+
+variable "assign_ip" {
+  description = "use assign ip"
+  type = bool
+  default = true
+}
+
+variable "additional_tags" {
+  default = {}
+  description = "Additional Resource Tags"
+  type = map(string)
+}
